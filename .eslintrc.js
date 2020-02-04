a691b5a1591aeb7a73750aa17d1b86f8d0960537
RamiTae <ramiTae.dev@gmail.com>
2020-02-04 21:55:15 +0900

    :construction: Doing initialize

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    // Airbnb style guide 적용
+    "airbnb-base",
+    // TypeScript ESLint recommanded style 적용
+    "plugin:@typescript-eslint/eslint-recommended",
+    "prettier"
+  ],
+  rules: {
+    "no-console": "off"
+  }
+};
