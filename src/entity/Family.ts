a0089ffe78476765e9f689ede2b45a03f6b26080
RamiTae <ramiTae.dev@gmail.com>
2020-02-09 02:39:08 +0900

    :card_file_box: Implement family table
    - familyName으로 family찾는 기능 구현

---
+++ src/entity/Family.ts
@@ -0,0 +1,24 @@
+import { Entity, BaseEntity, PrimaryColumn, Column, OneToMany } from "typeorm";
+import Plant from "./Plant";
+
+@Entity()
+export default class Family extends BaseEntity {
+  @PrimaryColumn({ unsigned: true })
+  id!: number;
+
+  @Column()
+  familyName!: string;
+
+  @OneToMany(
+    (type) => Plant,
+    (plant) => plant.family,
+  )
+  plants!: Plant[];
+
+  // ? familyName 토대로 family찾음(커뮤니티 기능)
+  static findByFamilyName(familyName: string): Promise<Family | undefined> {
+    return this.createQueryBuilder("family")
+      .where("family.familyName = :familyName", { familyName })
+      .getOne();
+  }
+}
