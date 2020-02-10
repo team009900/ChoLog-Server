782d778e1212436622210f86960cb47cab0bf0e5
RamiTae <ramiTae.dev@gmail.com>
2020-02-10 17:38:40 +0900

    :bug: Comment out due to compile error

--- src/passport/index.ts
+++ src/passport/index.ts
@@ -1,5 +1,5 @@
-import * as local from "./localStrategy";
-import * as kakao from "./kakaoStrategy";
-import Users from "../entity/users";
+// import * as local from "./localStrategy";
+// import * as kakao from "./kakaoStrategy";
+// import Users from "../entity/users";
 
 export default (passport: any) => {};
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,19 +1,19 @@
 import * as express from "express";
 
-import * as authController from "../controller/auth";
-import { isLoggedIn, isNotLoggedIn } from "../middlewares";
+// import * as authController from "../controller/auth";
+// import { isLoggedIn, isNotLoggedIn } from "../middlewares";
 
 const router = express.Router();
 
 // router.use(isNotLoggedIn);
 
-// * POST /user/signup
-router.post("/signup", isNotLoggedIn, authController.signup.post);
+// // * POST /user/signup
+// router.post("/signup", isNotLoggedIn, authController.signup.post);
 
-// * POST  /user/login
-router.post("/login", isNotLoggedIn, authController.login.post);
+// // * POST  /user/login
+// router.post("/login", isNotLoggedIn, authController.login.post);
 
-// * POST /user/logout
-router.post("/logout", isLoggedIn, authController.logout.post);
+// // * POST /user/logout
+// router.post("/logout", isLoggedIn, authController.logout.post);
 
 export default router;
