61b45cce247d9a436d7f3380aaf4cf480247678e
RamiTae <ramiTae.dev@gmail.com>
2020-02-18 23:19:45 +0900

    :zap: Implement token decode in isLoggedIn
    - 이 미들웨어를 거친 후 토큰 내용에 편하게 접근하게 하기 위해서 추가. (<any>req).decoded 를 사용하면 디코딩 된 토큰에 접근 가능

--- src/middlewares/isLoggedIn.ts
+++ src/middlewares/isLoggedIn.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from "express";
+import * as jwt from "jsonwebtoken";
 import Blacklist from "../entity/Blacklist";
 
 export default async (req: Request, res: Response, next: NextFunction) => {
@@ -9,15 +10,24 @@ export default async (req: Request, res: Response, next: NextFunction) => {
 
     try {
       if (blacklistToken) {
-        res
-          .status(401)
-          .json("토큰 유효기간이 만료되었습니다. 다시 로그인 해주세요.");
+        res.status(401).json("토큰 유효기간이 만료되었습니다. 다시 로그인 해주세요.");
         return;
       }
     } catch (error) {
       res.status(400).json(error);
       return;
     }
+
+    if (token === undefined) {
+      res.status(400).json("plz send authorization");
+      return;
+    }
+
+    const jwtSecret: string = process.env.JWT_SECRET ? process.env.JWT_SECRET : "";
+
+    const decoded: any = jwt.verify(token.substring(7), jwtSecret);
+    (<any>req).decoded = decoded; // next에서 decoded된 값들을 사용할 수 있게 저장
+
     next();
   } else {
     res.status(403).send("로그인 해주세요");
