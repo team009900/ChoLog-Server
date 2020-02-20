be51818000a2735810240c433f60e1569ca98037
RamiTae <ramiTae.dev@gmail.com>
2020-02-20 15:13:04 +0900

    :zap: Add index file in entity directory

---
+++ src/entity/index.ts
@@ -0,0 +1,27 @@
+import API from "./API";
+import Blacklist from "./Blacklist";
+import Diary from "./Diary";
+import Family from "./Family";
+import Parameter from "./Parameter";
+import Plant from "./Plant";
+import PlantDataImg from "./PlantDataImg";
+import PlantDetail from "./PlantDetail";
+import PlantsDatabase from "./PlantsDatabase";
+import State from "./State";
+import User from "./User";
+import Weather from "./Weather";
+
+export {
+  API,
+  Blacklist,
+  Diary,
+  Family,
+  Parameter,
+  Plant,
+  PlantDataImg,
+  PlantDetail,
+  PlantsDatabase,
+  State,
+  User,
+  Weather,
+};
