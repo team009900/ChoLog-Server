acba8219c7c0ebc1af144b0b8ffb927a843efd4a
RamiTae <ramiTae.dev@gmail.com>
2020-02-20 15:14:31 +0900

    :sparkles: Add diary data formatting function

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,5 @@
 import deleteImg from "./deleteImg";
+import diaryFormatting from "./diaryFormatting";
 
 // eslint-disable-next-line import/prefer-default-export
-export { deleteImg };
+export { deleteImg, diaryFormatting };
