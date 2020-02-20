28b1efb485728bfd2d3c5418eb4932418ea53aa9
RamiTae <ramiTae.dev@gmail.com>
2020-02-21 00:40:34 +0900

    :construction: Implement basic search logic

--- src/controller/plantsdb/getSearch.ts
+++ src/controller/plantsdb/getSearch.ts
@@ -1,6 +1,20 @@
 import { Request, Response, NextFunction } from "express";
+import { PlantsDatabase } from "../../entity";
 
-export default (req: Request, res: Response, next: NextFunction): void => {
-  const { q: target } = req.query;
-  res.json(`plantsDB search. target: ${typeof target} ${target}`);
+export default async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  try {
+    const { q: target } = req.query;
+    console.log(`plantsDB search. target: ${typeof target} ${target}`);
+    if (target === undefined) {
+      res.status(400).json("Plz send query");
+      return;
+    }
+
+    const dataList = await PlantsDatabase.findPlantsDataList(target);
+    // console.log(dataList);
+    res.json(dataList);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json(`Error name: ${err.name}`);
+  }
 };
