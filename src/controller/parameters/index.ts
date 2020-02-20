c57867543099dddf730da12deb74da3959e009dd
Hiz <kljuyt107@gmail.com>
2020-02-20 21:02:27 +0900

    :sparkles: Implement Parameter getAll API

--- src/controller/parameters/index.ts
+++ src/controller/parameters/index.ts
@@ -1,10 +1,19 @@
 import { Request, Response, NextFunction } from "express";
+import Plant from "../../entity/Plant";
+import Parameter from "../../entity/Parameter";
 import postPlant from "./postPlant";
 import patchPlant from "./patchPlant";
 import getPlant from "./getPlant";
 
-const getAll = (req: Request, res: Response, next: NextFunction): void => {
-  res.json("all parameters get.");
+const getAll = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  try {
+    const allParameters: Parameter[] = await Parameter.find();
+    res.status(200).json(allParameters);
+    return;
+  } catch (error) {
+    console.error(error);
+    res.status(400).json(`Error name: ${error.name}`);
+  }
 };
 
 export { postPlant, getPlant, getAll, patchPlant };
