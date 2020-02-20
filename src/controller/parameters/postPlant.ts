7902188a8ed03eeffc44aee873560f7eb0e89ad7
Hiz <kljuyt107@gmail.com>
2020-02-21 03:45:39 +0900

    :sparkles: Implement Parameter postPlant API

--- src/controller/parameters/postPlant.ts
+++ src/controller/parameters/postPlant.ts
@@ -1,6 +1,58 @@
 import { Request, Response, NextFunction } from "express";
+import Plant from "../../entity/Plant";
+import Parameter from "../../entity/Parameter";
 
-export default (req: Request, res: Response, next: NextFunction): void => {
+export default async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   const { plantId } = req.params;
-  res.json(`plant parameters post. plantId: ${plantId}`);
+  if (!Number(plantId)) {
+    res.status(400).json("You send us bad request");
+  }
+
+  const { parameters } = req.body; // "[1, 2, 3]"
+  if (parameters.charAt(0) !== "[") {
+    res.status(400).json("Array 형식으로 입력해 주세요.");
+    return;
+  }
+
+  const parseParameters = JSON.parse(parameters); // [1, 2, 3]
+
+  if (!Array.isArray(parseParameters)) {
+    res.status(400).json("Array 형식으로 입력해 주세요.");
+    return;
+  }
+
+  for (let i = 0; i < parseParameters.length; i + 1) {
+    if (typeof parseParameters[i] !== "number") {
+      res.status(400).json("Array안에는 숫자만 입력 가능합니다.");
+      return;
+    }
+  }
+
+  try {
+    const findPlant = await Plant.findOne({
+      where: { id: plantId, relations: ["parameters"] },
+    });
+    if (findPlant === undefined) {
+      res.status(404).json(`Plant ${plantId} does not exist`);
+      return;
+    }
+
+    const selectParameters = await Promise.all(
+      parseParameters.map((id: number) => Parameter.findOne(id)),
+    )
+      .then((result: any) => result)
+      .catch((err) => {
+        console.error(err);
+        res.status(400).json(`Error: ${err}`);
+      });
+
+    findPlant.parameters = selectParameters;
+    await Plant.save(findPlant);
+
+    res.status(201).json(selectParameters);
+    return;
+  } catch (error) {
+    console.error(error);
+    res.status(400).json(`Error: ${error}`);
+  }
 };
