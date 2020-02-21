3a8b523b4863082c6f9f7cbbea14287166f2dac7
RamiTae <ramiTae.dev@gmail.com>
2020-02-21 23:46:42 +0900

    :art: Make modularization setMyTimer

--- src/services/index.ts
+++ src/services/index.ts
@@ -4,5 +4,14 @@ import setImgDelQuery from "./setImgDelQuery";
 import removeNullKeys from "./removeNullKeys";
 import makeXmlToStr from "./makeXmlToStr";
 import requestGetBody from "./requestGetBody";
+import setMyTimer from "./setMyTimer";
 
-export { deleteImg, diaryFormatting, setImgDelQuery, removeNullKeys, makeXmlToStr, requestGetBody };
+export {
+  deleteImg,
+  diaryFormatting,
+  setImgDelQuery,
+  removeNullKeys,
+  makeXmlToStr,
+  requestGetBody,
+  setMyTimer,
+};
